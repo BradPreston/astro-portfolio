f0d9f91b1262ef8e2505e99d70b4407cddeaf528
Brad Preston <bap5393@gmail.com>
2026-04-24 22:23:38 -0400

    configure astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bradpreston.com',
+  trailingSlash: "always",
   integrations: [mdx()]
 });
